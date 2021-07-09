ae55444614364b8f45e7ac5d79cbcc4ef2aeae02
Jebakani <vjishwarya@gmail.com>
2021-07-09 15:18:43 +0530

    [JEBAKANI] UC1-Creating simple linked list

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LinkedList
 {
     class Program
     {
+        /// <summary>
+        /// UC1-Creating simple Linked List 
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Implementation");
+            //creating object for collection class linked list 
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddFirst(70);
+            linkedList.AddLast(58);
+            linkedList.AddLast(30);
+            foreach (int i in linkedList)
+            {
+                Console.WriteLine(i);
+            }
             Console.Read();
         }
     }
