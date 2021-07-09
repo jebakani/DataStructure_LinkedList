dda9e40660318db4378b60a3c2ec64a44cbb34cb
Jebakani <vjishwarya@gmail.com>
2021-07-09 15:28:56 +0530

    [JEBAKANI] UC3-Adding method to insert node at last

--- LinkedList/MyLinkedList.cs
+++ LinkedList/MyLinkedList.cs
@@ -38,6 +38,27 @@ namespace LinkedList
             }
         }
 
+        public void AddLast(T value)
+        {
+            //creating the new node with given value
+            Node<T> newNode = new Node<T>(value);
+            //check whether the head is null or not . 
+            if (head == null)
+            {
+                head = newNode;
+            }
+            //iterate till the last element and make next feild of last element as newNode
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNode;
+            }
+        }
+
         //method to display the element 
         public void DisplayList()
         {
